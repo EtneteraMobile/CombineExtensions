7fb75d53d9b73c67f949500ecee9779979feced0
Tuan Tu Do <tuanthatsmyname@gmail.com>
2021-10-04 22:58:26 +0100

    Added new extension for sink, when the Failure is Never

--- Sources/CombineExtensions/Combine+/Publisher+sink.swift
+++ Sources/CombineExtensions/Combine+/Publisher+sink.swift
@@ -40,3 +40,20 @@ public extension Publisher {
         )
     }
 }
+
+public extension Publisher where Failure == Never {
+    func sink<A: AnyObject>(
+        weak object: A,
+        logger: @escaping (String) -> Void = { Swift.print($0) },
+        in file: String = #file,
+        on line: Int = #line,
+        receiveValue: @escaping (A, Self.Output) -> Void
+    ) -> AnyCancellable {
+        sink(
+            weak: object,
+            logger: logger,
+            receiveCompletion: { _, _ in },
+            receiveValue: receiveValue
+        )
+    }
+}
